6c238e8084ba755dad96d081fc499d755980a68b
Cristian-VA <132118380+Cristian-VA@users.noreply.github.com>
2024-01-23 16:47:18 -0500

    feat: appwrite integration

--- NetPro/src/Root/Layout/Navigation/MobileNav.tsx
+++ NetPro/src/Root/Layout/Navigation/MobileNav.tsx
@@ -8,12 +8,12 @@ const MobileNav = ({toggleIsOpen}:{toggleIsOpen:() => void}) => {
     
    
     <motion.div
-    initial={{ x: '-100%' }}  // Initially positioned outside the viewport on the left
+    initial={{ x: '-100%' }}  
       animate={{ x: 0 }}      
       exit={{ x: '-100%' }}      
       transition={{ duration: 0.3 }}
 
-      className="absolute  z-20  w-screen top-[60px] px-7 py-5 md:hidden  overflow-scroll bg-gray-50 " 
+      className="absolute  z-50  w-screen top-[60px] px-7 py-5 md:hidden  overflow-scroll bg-gray-50 " 
       style={{ height: 'calc(100vh - 60px)' }}
      
     >
